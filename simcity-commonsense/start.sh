61213381b9e7a29ddd454074312f4afea120155e
Berend Weel <b.weel@esciencecenter.nl>
2017-05-17 14:04:48 +0200

    Moved default location of csWeb and sim-city-cs to a simcity directory

--- simcity-commonsense/start.sh
+++ simcity-commonsense/start.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-cd /home/simcity/sim-city-cs
+cd /home/simcity/simcity/csWeb
+npm install
+bower install
+gulp init
+bower link
+cd out/csServerComp
+npm link
+
+cd /home/simcity/simcity/sim-city-cs
 npm install
 npm link csweb
-cd /home/simcity/sim-city-cs/public
+cd /home/simcity/simcity//sim-city-cs/public
 bower install
 bower link csweb
-cd /home/simcity/sim-city-cs
+cd /home/simcity/simcity/sim-city-cs
 typings install
+gulp init
+sleep 5s
 gulp serve
