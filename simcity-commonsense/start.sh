3aac56cf3be834bb1c5885db062d88bcc4729767
Carlos Martinez <c.martinez@esciencecenter.nl>
2017-07-12 13:54:52 +0200

    Demo scenario for common sense stack

--- simcity-commonsense/start.sh
+++ simcity-commonsense/start.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
-cd /home/simcity/simcity/csWeb
-npm install
-bower install
-gulp init
-bower link
-cd out/csServerComp
-npm link
-#
 cd /home/simcity/simcity/sim-city-cs
-npm install
-npm link csweb
-cd /home/simcity/simcity/sim-city-cs/public
-bower install
-bower link csweb
-cd /home/simcity/simcity/sim-city-cs
-typings install
-gulp init
-sleep 5s
 gulp serve
---
+++ simcity-slurm/simulations/mysim/makeCircle.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import numpy as np
+
+inputsFile = sys.argv[1]
+inputs = json.load(open(inputsFile))
+
+try:
+    x = float(inputs['centre'][0]['x'])
+    y = float(inputs['centre'][0]['y'])
+    r = float(inputs['radius'])
+except:
+    print('Could not read centre / radius, using default values', file=sys.stderr)
+    x = 0
+    y = 0
+    z = 10
+
+coords = [  ]
+
+for t in np.linspace(0, 2*np.pi, 50):
+    h = np.round(x + np.cos(t) * r,4)
+    k = np.round(y + np.sin(t) * r,4)
+    coords.append([ h, k ])
+
+geojson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          coords
+        ]
+      },
+      "properties": {},
+      "id": "xxxx_-8000"
+    }
+  ]
+}
+
+print(json.dumps(geojson))
