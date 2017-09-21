27b1adbb026f96ad629e6411f988f83e782341af
Carlos <c.martinez@esciencecenter.nl>
2017-09-21 19:12:08 +0200

    Fix webdav issues

--- simcity-webservice/start.sh
+++ simcity-webservice/start.sh
@@ -2,16 +2,16 @@
 
 # TODO: Make user able to set the task and job db's here, or use the config.ini
 . env/bin/activate
-while ! curl -s http://taskdb:5984/
+while ! curl -s http://backend:5984/
 do
   echo "Waiting for task db..."
   sleep 1
 done
-while ! curl -s http://jobdb:5984/
-do
-  echo "Waiting for job db..."
-  sleep 1
-done
+# while ! curl -s http://jobdb:5984/
+# do
+#   echo "Waiting for job db..."
+#   sleep 1
+# done
 TIME=`date`
 echo "$TIME - connected successfully"
 
